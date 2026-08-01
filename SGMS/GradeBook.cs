aba4a36cc85d0915daae7513bf24ed46af0a9ae1
Ahmeddouhm <a.douha0@gmail.com>
2026-08-01 23:52:43 +0300

    Add SGMS Classes

---
+++ SGMS/GradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGMS
+{
+    internal class GradeBook
+    {
+        public string? ClassName { get; set; }
+        public List<Student>? Students { get; set; }
+
+        public GradeBook(string className)
+        {
+            ClassName = className;
+            Students = new();
+        }
+    }
+}
---
+++ SGMS/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGMS
+{
+    internal class Student
+    {
+        public string? ID { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public Dictionary<string, double>? Grades { get; set; }
+
+        public Student(string id , string name , string email)
+        {
+            ID = id;
+            Name = name;
+            Email = email;
+            Grades = new();
+        }
+
+
+    }
+
+    
+}
