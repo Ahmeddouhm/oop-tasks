d6485ec235dd015da4db9bd1444471e8031439c6
Ahmeddouhm <a.douha0@gmail.com>
2026-08-07 17:23:17 +0300

    2 Classes Of 4 Basic Init

---
+++ VRS/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRS
+{
+    internal class Customer
+    {
+        public string? ID { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? DriversLicenseNumber { get; set; }
+
+        public Customer(string id,string name,string phoneNumber,string email,string driverLicenseNumber)
+        {
+            ID = id;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            DriversLicenseNumber = driverLicenseNumber;
+        }
+
+        public string GetCustomerInfo() 
+        {
+            var sb = new StringBuilder();
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ VRS/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRS
+{
+    internal class Vehicle
+    {
+        public string? ID { get; set; }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public int Year { get; set; }
+        public double DailyRate { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Vehicle(string id, string make, string model, int year, double dailyrate)
+        {
+            ID = id;
+            Make = make;
+            Model = model;
+            Year = year;
+            DailyRate = dailyrate;
+            IsAvailable = true;
+        }
+
+
+        public void Rent() 
+        {
+            IsAvailable = false;
+        }
+
+        public void ReturnVehicle() 
+        {
+            IsAvailable = true;
+        }
+
+        public double CalculateRentalCost(int days) 
+        {
+            return DailyRate * days;
+        }
+        public string GetVehicleInfo()
+        {
+            var sb = new StringBuilder();
+
+            return sb.ToString();
+        }
+    }
+}
