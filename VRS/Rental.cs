fd0a728508edd4a7f2ad9b0d7794e21602112e8f
Ahmeddouhm <a.douha0@gmail.com>
2026-08-10 18:13:56 +0300

    Only Basic Init

--- VRS/Rental.cs
+++ VRS/Rental.cs
@@ -6,5 +6,44 @@ namespace VRS
 {
     internal class Rental
     {
+        public string ID { get; set; }
+        public Customer Customer { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public Rental(string id, Customer customer, Vehicle vehicle, DateTime startDate, DateTime endDate)
+        {
+            ID = id;
+            Customer = customer;
+            Vehicle = vehicle;
+            StartDate = startDate;
+            EndDate = endDate;
+            IsActive = true;
+        }
+
+        public int GetRentalDuration() 
+        {
+            return (EndDate - StartDate).Days;
+        }
+
+        public double GetTotalCost() 
+        {
+            return Vehicle.CalculateRentalCost(GetRentalDuration()) + 250;
+        }
+
+        public void CompleteRental() 
+        {
+            Vehicle.ReturnVehicle();
+            IsActive = true;
+        }
+
+        public string GetRentalInfo() 
+        {
+            var sb = new StringBuilder();
+
+            return sb.ToString();
+        }
     }
 }
--- VRS/RentalAgency.cs
+++ VRS/RentalAgency.cs
@@ -6,5 +6,62 @@ namespace VRS
 {
     internal class RentalAgency
     {
+        public string AgencyName { get; set; }
+        public List<Vehicle> Vehicles { get; set; }
+        public List<Customer> Customers { get; set; }
+        public List<Rental> Rentals { get; set; }
+
+        public RentalAgency(string agencyName)
+        {
+            AgencyName = agencyName;
+            Vehicles = new();
+            Customers = new();
+            Rentals = new();
+        }
+
+        public void AddVehicle(Vehicle vehicle) 
+        {
+        
+        }
+
+        public void RegisterCustomer(Customer customer) { }
+
+
+        public void CreateRental(Customer customer, Vehicle vehicle, int days)
+        {
+
+        }
+
+        public void CompleteRental(string rentalId)
+        {
+
+        }
+        public List<Vehicle> GetAvailableVehicles() 
+        {
+            List<Vehicle> res = new();
+
+            return res;
+        }
+
+        public List<Rental> GetActiveRentals()
+        {
+            List<Rental> res = new();
+
+            return res;
+        }
+
+        public List<Customer> getCustomerRentals(string customerId)
+        {
+            List<Customer> res = new();
+
+            return res;
+        }
+
+        public string DisplayFleet()
+        {
+            var sb = new StringBuilder();
+
+            return sb.ToString();
+        }
     }
 }
